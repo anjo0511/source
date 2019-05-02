b7442248cb441e5f51b5aad1fd4bd1ede4141770
anjo0511 <39155159+anjo0511@users.noreply.github.com>
2019-05-02 22:00:42 +0200

    Update vennPlot.aj.R

--- vennPlot.aj.R
+++ vennPlot.aj.R
@@ -15,3 +15,18 @@ vennPlot.aj <- function(list.with.names, title) {
       )
     )
   }
+#
+#
+#
+indiv.inter.aj  <- function(list,name) {
+    # Given a list of three elements with names
+    # this finds the individual elements of the spec name.
+    require(rlist)
+    name.inx <- match(name,names(list))
+    new.list = list.remove(list,name.inx)
+    first = list[[name.inx]]
+    second = new.list[[1]]
+    third = new.list[[2]]
+    uniq.for.first <- setdiff(first,union(second,third))
+    return(uniq.for.first)
+}

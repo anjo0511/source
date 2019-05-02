b81d350d8cfc2ce4ed34ad30f585b62c9bbe9067
anjo0511 <39155159+anjo0511@users.noreply.github.com>
2019-05-02 18:23:55 +0200

    Create vennPlot.aj.R

---
+++ vennPlot.aj.R
@@ -0,0 +1,16 @@
+#
+#
+vennPlot.aj <- function(list.with.names, title) {
+    require(RColorBrewer);require(VennDiagram)
+    pal <- brewer.pal(8, "Dark2")
+    grid.newpage()
+    grid.draw(
+      venn.diagram(
+        list.with.names,
+        filename = NULL,
+        fill = pal[1:length(list.with.names)],
+        cat.pos = c(335, 30, 180), 
+        main = title
+      )
+    )
+  }

0414df07e551f6b905a15ba56ba869d9810d9ebc
anjo0511 <39155159+anjo0511@users.noreply.github.com>
2019-05-04 00:34:53 +0200

    Update vennPlot.aj.R

--- vennPlot.aj.R
+++ vennPlot.aj.R
@@ -1,32 +1,36 @@
 #
 # Written by: Andreé Johnsson,andreejohnsson@outlook.com
 #
-vennPlot.aj <- function(list.with.names, title) {
+vennPlot.aj <- function(list.with.names, title="") {
+    futile.logger::flog.threshold(
+        futile.logger::ERROR,name = "VennDiagramLogger") # Supresses Logfiles
     require(RColorBrewer);require(VennDiagram)
     pal <- brewer.pal(8, "Dark2")
     grid.newpage()
     grid.draw(
-      venn.diagram(
-        list.with.names,
-        filename = NULL,
-        fill = pal[1:length(list.with.names)],
-        cat.pos = c(335, 30, 180), 
-        main = title
-      )
+        venn.diagram(
+            list.with.names,
+            filename = NULL,
+            fill = pal[1:length(list.with.names)],
+            # cat.pos = c(335, 30, 180), 
+            main = title,
+            euler.d  = TRUE,
+            scaled = TRUE,
+            cex = 1.25,
+            cat.cex = 1.25,
+            main.cex = 1.34,
+            cat.dist = 0.05,
+            na = "remove",
+            force.unique = TRUE
+        )
     )
-  }
-#
-#
-#
-indiv.inter.aj  <- function(list,name) {
-    # Given a list of three elements with names
-    # this finds the individual elements of the spec name.
-    require(rlist)
-    name.inx <- match(name,names(list))
-    new.list = list.remove(list,name.inx)
-    first = list[[name.inx]]
-    second = new.list[[1]]
-    third = new.list[[2]]
-    uniq.for.first <- setdiff(first,union(second,third))
-    return(uniq.for.first)
+    
 }
+#
+# TEST
+# l.1 = list("First"=sample(seq(3200),1),
+#            "Second"=sample(seq(3200),1110),
+#            "Third"=sample(seq(3200),212))
+# 
+# vennPlot.aj(l.1,title = "Venn Diagram Title")
+

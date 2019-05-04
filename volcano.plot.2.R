a0c7b3c8c1588e8d73b28e2585a7010a3ac0e96a
anjo0511 <39155159+anjo0511@users.noreply.github.com>
2019-05-04 18:14:32 +0200

    Update volcano.plot.2.R

--- volcano.plot.2.R
+++ volcano.plot.2.R
@@ -2,7 +2,7 @@
 #
 #
 #
-volcano.plot.2 <- function(res.df, title="", use.labels=FALSE) {
+volcano.plot.2 <- function(res.df, title="Volcano Plot", use.labels=FALSE) {
     # res.df is a regular df with at least the columns
     # log2FoldChange and padj 
     require(ggplot2);require(ggrepel)
@@ -16,13 +16,25 @@ volcano.plot.2 <- function(res.df, title="", use.labels=FALSE) {
                    (padj <= .01 & !is.na(padj) & abs(log2FoldChange) >= 0.5)
                    , "Yes", "No") )
     res.df$Significance %<>% factor(levels =c("Yes","No") )
+    
+    res.df.sig <- filter(res.df,Significance=="Yes")
+    up.down <-
+        res.df.sig %>%
+        summarise(Up = sum(log2FoldChange>0, na.rm = TRUE),
+                  Down = sum(log2FoldChange<0, na.rm = TRUE) )
+    not.sig.label <- paste(c("Not Sig: "),
+                             nrow(res.df)-sum(up.down),
+                             collapse = "")
+    up.down <- paste(c("    Up: ","\nDown: "),up.down,collapse = "")
+    
     p.1 <- res.df %>% 
         ggplot(aes(x =log2FoldChange, y =neg.log.10.padj,
                    fill = Significance) ) +
-        geom_point(shape = 21, col="white", size=3, alpha=0.5) + 
+        geom_point(shape = 21, col="white", size=3, alpha=0.7) + 
         theme_light(base_size = 12) + 
         theme(legend.position = "top") +
-        scale_fill_manual(values=c("red", "blue") ) +
+        scale_fill_manual(values=c("red", "blue"),
+                          labels = c(up.down, not.sig.label) ) +
         labs(x="Log2FoldChange", y="- Log10 (padj-value)",
              title = title, 
              caption = "\nThreshold: padj <= 0.01 & Log2FoldChange >= 0.5") +
@@ -30,9 +42,8 @@ volcano.plot.2 <- function(res.df, title="", use.labels=FALSE) {
                    linetype="dashed", color = "black") +
         geom_vline(xintercept=c(-log2(2^0.5),log2(2^0.5)),
                    linetype="dashed", color = "black")
-    
+        
     if (use.labels == TRUE) {
-        res.df.sig = filter(res.df,Significance=="Yes")
         p.1 = p.1 +
             geom_text_repel(data= res.df.sig,
             aes(label = res.df.sig$genes),
@@ -42,5 +53,3 @@ volcano.plot.2 <- function(res.df, title="", use.labels=FALSE) {
     }
     return(p.1)
 }
-
-

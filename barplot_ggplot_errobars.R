ec4e1df5e0c21ba03005d2df4ea2b88fe787dbc9
anjo0511 <39155159+anjo0511@users.noreply.github.com>
2019-05-10 21:00:36 +0200

    Update barplot_ggplot_errobars.R

--- barplot_ggplot_errobars.R
+++ barplot_ggplot_errobars.R
@@ -2,49 +2,34 @@
 # Written by: Andreé Johnsson,andreejohnsson@outlook.com
 #
 #
-bar.plot.aj <- function(df, grouping.var, label.vector = NULL) {
-  # Returns a barchart with standard error bars, by specidying a grouping
-  # variable given a tidy dataframe of only the geouping variable and the
-  # data to be plotted.
-  require(magrittr)
-  require(tidyverse)
-  grouping.var <- enquo(grouping.var)
-  df.sum <-
-    df %>%
-    group_by(!!grouping.var) %>%
-    summarise_if(is.double, list(mean = mean, sd = sd)) %>%
-    gather(., measure, value, 2:ncol(.)) %>%
-    separate(measure, into = c("measure", "statistic"), sep = "_") %>%
-    spread(statistic, value) %>%
-    mutate(
-      seer.up = mean + sd / sqrt(mean),
-      seer.down = mean - sd / sqrt(mean)
-    )
-  p.1 <-
-    df.sum %>%
-    ggplot(data = ., aes(
-      x = measure, y = mean,
-      group = (!!grouping.var), fill = (!!grouping.var)
-    )) +
-    geom_bar(
-      stat = "identity", color =
-        "black", position = position_dodge()
-    ) +
-    geom_errorbar(aes(ymin = seer.down, ymax = seer.up),
-      width = .2,
-      position = position_dodge(.9)
-    ) +
-    theme_bw() +
-    scale_fill_brewer(palette = "Spectral")
-
-  if (!is.null(label.vector)) {
-    p.1 <- p.1 + labs(
-      x = label.vector[1], y = label.vector[2],
-      title = label.vector[3]
-    )
-  }
-
-  return(p.1)
+barPlot.aj <- function(df, group) {
+    require(magrittr);require(tidyverse)
+    #
+    # Returns a barchart with standard error bars, by specidying a grouping
+    # variable given a tidy dataframe of only the geouping variable and the
+    # data to be plotted.
+    names(group)= NULL
+    df.mean <- df %>% 
+        select_if(is.numeric) %>% aggregate(group,mean)
+    df.sd <- df %>% 
+        select_if(is.numeric) %>% aggregate(group,sd)
+    df.sd[-1] <- df.sd[-1]/sqrt(table(unlist(group))) # This specifies the stadard error
+    
+    df.mean  %<>% gather(type,mean, -Group.1)
+    df.sd %<>% gather(type,se, -Group.1) # This names the stadard error,se
+    df.full <- inner_join(df.mean,df.sd,by =c("type","Group.1"))
+    
+    p.1 <-
+        df.full %>%
+        ggplot(data=., aes(x=type, y=mean,mgroup=Group.1, fill= Group.1)) +
+        geom_bar(stat = "identity", color = "black", 
+                 position = position_dodge() ) +
+        geom_errorbar(aes(ymax = mean+se, ymin = mean-se),
+                      width = .2, position = position_dodge(.9) ) +
+        theme_bw() +
+        scale_fill_brewer(palette = "Spectral") 
+    
+    return(p.1)
 }
 # Example 
 # ex.iris <-
